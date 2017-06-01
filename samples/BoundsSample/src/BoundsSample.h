c88518c7a6e511129b7b38e5b83d4fb4f3b1b729
Mark van de Korput <mark.korput@gmail.com>
2017-06-01 13:18:59 +0200

    cmake files for all other sample projects

--- samples/BoundsSample/src/BoundsSample.h
+++ samples/BoundsSample/src/BoundsSample.h
@@ -12,29 +12,29 @@ class BoundsSample
 {
 public:
     static BoundsSampleRef create();
-    
+
     void setup();
-    
+
 protected:
     void update();
-	
+
 private:
     ci::TextBox mInfoText;
     po::scene::TextBoxRef mWindowTextBox;
-    
+
     po::scene::ShapeRef mBG;
     po::scene::TextBoxRef mTextBox;
-    
+
     po::scene::NodeContainerRef mNodeContainer;
     po::scene::ShapeRef mNodeContainerBG;
     po::scene::TextBoxRef mNodeContainerTextBox;
-    
-    
+
+
     void nodeMouseOver(po::scene::MouseEvent &event);
     po::scene::NodeRef mSelectedNode;
-    
+
     void keyPressed(ci::app::KeyEvent &key);
-    
+
     std::string getNodeInfo(po::scene::NodeRef);
 
 };
